ba2313e440126db88d877e7b74e20cb276f5884b
Mariana Marques <marianadacunhamarques@gmail.com>
2020-11-09 17:30:04 -0300

    first CSS adjustments

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ setInterval(function load() {
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
 
+    // Display a greeting accordingly to the current time
+    if (hour >= 0 && hour < 12) {
+        document.getElementById("greeting").innerHTML = "Good morning!";
+    } else if (hour >= 12 && hour < 18) {
+        document.getElementById("greeting").innerHTML = "Good afternoon!";
+    } else {
+        document.getElementById("greeting").innerHTML = "Good evening!";
+    }
+
     // If the number of hours/minutes/seconds is less than 10, this will add a '0' at the beginning
     // so the number will be a proper hexadecimal code with a total of 6 digits
     hexHour = (hour < 10 ? '0' : '') + hour + ":" +
@@ -18,12 +27,4 @@ setInterval(function load() {
 
     window.document.getElementById('clock').innerHTML = hexHour;
     window.document.body.style.backgroundColor = hexColor;
-
-    if (hour >= 0 && hour < 12) {
-        document.getElementById("greeting").innerHTML = "Good morning!";
-    } else if (hour >= 12 && hour < 18) {
-        document.getElementById("greeting").innerHTML = "Good afternoon!";
-    } else {
-        document.getElementById("greeting").innerHTML = "Good evening!";
-    }
 }, 1)
